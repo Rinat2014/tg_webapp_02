d867bf5150890193350e4704e2b988473d34fe33
yagafarovrinat <yagafarovrinat@gmail.com>
2026-01-07 07:07:42 +0500

    pravki

--- app/components/layout/Navigation/MainNav.vue
+++ app/components/layout/Navigation/MainNav.vue
@@ -2,7 +2,7 @@
 <div class="fixed bottom-0 left-0 right-0 z-40">
   <ul class="bg-blue-500/10 h-20 text-xl   flex justify-around items-center ">
     <li><NuxtLink  class="text-gray-700 hover:bg-blue-500/40   dark:text-white dark:hover:bg-blue  p-5 rounded" to="/">Home</NuxtLink></li>
-    <li><NuxtLink  class="text-gray-700 hover:bg-blue-500/40   dark:text-white dark:hover:bg-blue  p-5 rounded" to="/about">About</NuxtLink></li>
+    <li><NuxtLink  class="text-gray-700 hover:bg-blue-500/40   dark:text-white dark:hover:bg-blue  p-5 rounded" to="/crm">CRM</NuxtLink></li>
     <li><NuxtLink  class="text-gray-700 hover:bg-blue-500/40   dark:text-white dark:hover:bg-blue  p-5 rounded" to="/user">User</NuxtLink></li>
     <li><NuxtLink  class="text-gray-700 hover:bg-blue-500/40   dark:text-white dark:hover:bg-blue  p-5 rounded" to="/pochasovka">Pochasovka</NuxtLink></li>
   </ul>
